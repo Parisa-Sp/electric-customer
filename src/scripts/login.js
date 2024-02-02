de27ac32a2c0ea1b3875a60a0aeb8e7b5918bcf7
Palili <parisasafipour1999@gmail.com>
2024-02-03 00:28:18 +0330

    feature: add login page

---
+++ src/scripts/login.js
@@ -0,0 +1,19 @@
+const button = document.getElementById("btn");
+const btnLoading = document.getElementById("btnLoading");
+const btnText = document.getElementById("btnText");
+const toast = document.getElementById("liveToast")
+  
+const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toast);
+
+button.onclick = (e) =>{
+    e.preventDefault();
+    
+    btnLoading.classList.toggle("d-none");
+    btnText.innerHTML = "چند لحظه صبر کنید";
+    toastBootstrap.show();
+
+    setTimeout(()=>{
+        btnLoading.classList.toggle("d-none");
+        btnText.innerHTML = "ورود به پنل";
+    }, 2000);
+}
